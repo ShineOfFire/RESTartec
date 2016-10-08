a0cb091ce89c7e55ef9470446261baa9759cf941
ShineOfFire <hakrya@hotmail.fr>
2016-10-08 22:54:14 +0200

    updated

--- README.md
+++ README.md
@@ -1,81 +1,90 @@
-# RESTartec SERVER/CLIENT
-![GitHub Logo](https://raw.githubusercontent.com/ShineOfFire/RESTartec/master/restartec.png)
-
-Api RESTartec for AngularJS (Ionic Framework)
-
 # RESTartec API USE ON CLIENT APP ANGULARJS (IONIC)
 
 For contact us [Webartec](http://webartec.fr/)
 
 RESTartec Api is an open source Javascript library for the AngularJS, Ionic API. With RESTartec API you have Javascript access to the database with own methods :
 
-## Next update
-* Add OAuth 2.0 server
-* Upload part client for use it
-
 ## Install
 
-Download ZIP
 * Download the zipfile from this project and install it.
-Git Clone 
-* Checkout the source: git clone https://github.com/ShineOfFire/RESTartec/ and install it yourself.
-Bower
-* Bower install RESTartec
+* Checkout the source: git clone git://github.com/** and install it yourself.
 
 ## Getting Started :
 
-* Install RESTartec 
+* Install RESTartec
 * find, set, update, remove, server
 
-## HOW TO USE
+## Examples
 
 **ELEMENT GET FIND EXAMPLE**
 
-* WITH ID - For one element
-```js
-Users.find(id);
-```
+	* WITH ID 		|	For one element
 
-* WITHOUT ID - For all elements
-```js
-Users.find();
-```
+		Users.find(id, ressource);
+
+	* WITHOUT ID 	|	For all elements
+
+		Users.find(0 ,ressource);
+
 **ELEMENT POST CREATE EXAMPLE**
 
-* INSERT ELEMENT WITH DATA PARAMS OBJECT
-```js
-var data = $.param({
-	login: 'toto'
-});
+	* INSERT ELEMENT WITH DATA PARAMS OBJECT
+	
+		var data = $.param({
+			    login: 'toto'
+			});
 
-Users.set(data);
-```
+		Users.set(data, ressource);
+
 **PUT UPDATE EXAMPLE**
 
-* UPDATE ELEMENT WITH ID AND DATA PARAMS OBJECT
-```js
-var data = $.param({
-	login: 'toto'
-});
+	* UPDATE ELEMENT WITH ID AND DATA PARAMS OBJECT
 
-Users.update(id, data);
-```
+		var data = $.param({
+			    login: 'toto'
+			});
+
+		Users.update(id, data, ressource);
+
 **DELETE CREATE EXAMPLE**
 
-* UPDATE ELEMENT WITH ID AND DATA PARAMS OBJECT
-```js
-Users.remove(id);
-```
+	* UPDATE ELEMENT WITH ID AND DATA PARAMS OBJECT
+
+		Users.remove(id, ressource);
+
 **GET SERVER INFO SESSION EXAMPLE**
 
-* GET SERVER INFORMATION
-```js
-Users.server();
-```
-* EXEMPLE FOR GET IP
-```js		
-var server = Users.server();
+	* IF GET IP WITH SERVER INFORMATION
 
-server.REMOTE_ADDR; //Return the client ip
-```
-*Create By Ryadh KRALFALLAH 27.06.2016*
+		Users.server();
+
+	* USE IT
+		
+		var server = Users.server();
+
+		server.REMOTE_ADDR; //Return the client ip
+
+## Documentation
+
+	Information server :
+		server: function()
+
+	Search element in database with ressource :
+		find: function(id = 0, ressource)
+
+	Insert element in database with ressource :
+		set: function(obj, ressource)
+
+	Update element in database with ressource :
+		update: function(id, obj, ressource)
+
+	Remove element in database with ressource :
+		remove: function(id, ressource)
+
+	* Description attributes :
+
+		id = index of element in database.
+		obj = this is the column of database, it's possible if one or multiple element.
+		ressource = is the table in database.
+
+*Create By Ryadh KRALFALLAH 27.05.2016*
--- index.php
+++ index.php
@@ -25,21 +25,39 @@
 
 	$application = new \Slim\Slim();
 
-	$application->get('/pics/get', 'all');					/* GET */
-	$application->get('/pics/get/:id', 'get');				/* GET :id */
-	$application->post('/pics/post', 'post');				/* CREATE */
-	$application->put('/pics/put/:id', 'put');				/* UPDATE :id */
-	$application->delete('/pics/delete/:id', 'delete');	/* DELETE :id */
-	$application->get('/server', 'server');	/* SERVER */
+	$application->get('/server', 'server');										/* SERVER */
+
+	$application->get('/users/get', 'allUsers');								/* GET */
+	$application->get('/users/get/:id', 'getUsers');							/* GET :id */
+	$application->post('/users/post', 'postUsers');								/* CREATE */
+	$application->put('/users/put/:id', 'putUsers');							/* UPDATE :id */
+	$application->delete('/users/delete/:id', 'deleteUsers');					/* DELETE :id */
+
+	$application->get('/livemaps/get', 'allLiveMaps');							/* GET */
+	$application->get('/livemaps/get/:id', 'getLiveMaps');						/* GET :id */
+	$application->post('/livemaps/post', 'postLiveMaps');						/* CREATE */
+	$application->put('/livemaps/put/:id', 'putLiveMaps');						/* UPDATE :id */
+	$application->delete('/livemaps/delete/:id', 'deleteLiveMaps');				/* DELETE :id */
+
+	$application->get('/clientSearch/get', 'allClientSearch');					/* GET */
+	$application->get('/clientSearch/get/:id', 'getClientSearch');				/* GET :id */
+	$application->post('/clientSearch/post', 'postClientSearch');				/* CREATE */
+	$application->put('/clientSearch/put/:id', 'putClientSearch');				/* UPDATE :id */
+	$application->delete('/clientSearch/delete/:id', 'deleteClientSearch');		/* DELETE :id */
+
+	/* CUSTOM FEATURE */
+	$application->delete('/livemaps/remove', 'remove');		    		/* REMOVE ALL OLD USERS */
 
 	$application->run();
 
 	/* DATABASE */
 	function getDB() {
+
+    	//webartec_server
 		$dbhost = "localhost";
 		$dbuser = "root";
 		$dbpass = "";
-		$dbname = "fhaare";
+		$dbname = "igo";
 
 		$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, array(
 				PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
@@ -53,13 +71,19 @@
 		echo json_encode($_SERVER);
 	}
 
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------- USERS ----------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+
 	/* GET */
-	function all() {
+	function allUsers() {
         $db = getDB();
  	 	$app = \Slim\Slim::getInstance();
 
  	 	$tab = $app->request->headers;
-		$table = 'pics'; //$tab['table'];
+		$table = 'users'; //$tab['table'];
 
  	 	try {
             $sth = $db->prepare("SELECT * FROM $table");
@@ -81,12 +105,12 @@
 	}
 
 	/* GET :id */
-	function get($id) {
+	function getUsers($id) {
 		$db = getDB();
 		$app = \Slim\Slim::getInstance();
 
 		$tab = $app->request->headers;
-		$table = 'pics';//$tab['table'];
+		$table = 'users';//$tab['table'];
 
 		try {
 			$sth = $db->prepare("SELECT * FROM $table WHERE id = :id");
@@ -110,13 +134,13 @@
 	}
 
 	/* CREATE */
-	function post() {
+	function postUsers() {
 		$db = getDB();
 		$app = \Slim\Slim::getInstance();
 
 		$tab = $app->request->post();
 
-		$table = 'pics';//$tab['table'];
+		$table = 'users';//$tab['table'];
 		unset($tab['_METHOD']);
 		unset($tab['table']);
 
@@ -157,13 +181,13 @@
 	}
 
 	/* UPDATE :id */
-	function put($id) {
+	function putUsers($id) {
 		$db = getDB();
 		$app = \Slim\Slim::getInstance();
 
 		$tab = $app->request->put();
 
-		$table = 'pics';//$tab['table'];
+		$table = 'users';//$tab['table'];
 		unset($tab['_METHOD']);
 		unset($tab['table']);
 
@@ -195,11 +219,11 @@
 	}
 
 	/* DELETE :id */
-	function delete($id) {
+	function deleteUsers($id) {
  		$db = getDB();
 		$app = \Slim\Slim::getInstance();
 		$tab = $app->request->delete();
-		$table = 'pics';//$tab['table'];
+		$table = 'users';//$tab['table'];
 
  		try {
 			$sth = $db->prepare("SELECT * FROM $table WHERE id = :id");
@@ -226,4 +250,399 @@
 				));
 		}
 	}
+
+
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*----------------------------------- LIVEMAPS ----------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+
+
+
+	/* GET */
+	function allLiveMaps() {
+        $db = getDB();
+ 	 	$app = \Slim\Slim::getInstance();
+
+ 	 	$tab = $app->request->headers;
+		$table = 'livemaps'; //$tab['table'];
+
+ 	 	try {
+            $sth = $db->prepare("SELECT * FROM $table");
+            $sth->execute();
+            $res = $sth->fetchAll(PDO::FETCH_OBJ);
+
+            if($res){
+	        	$app->response->setStatus(200);
+	        	$app->response()->headers->set('Content-Type', 'application/json');
+	            echo json_encode($res);
+	            $db = null;
+            } else {
+            	throw new PDOException('No records found.');
+            }
+        } catch (PDOException $e) {
+        	$app->response->setStatus(404);
+            echo '{"error":{"text":' . $e->getMessage() . '}}';
+        }
+	}
+
+	/* GET :id */
+	function getLiveMaps($id) {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+
+		$tab = $app->request->headers;
+		$table = 'livemaps';//$tab['table'];
+
+		try {
+			$sth = $db->prepare("SELECT * FROM $table WHERE id = :id");
+			$sth->execute(array(
+					'id'   	=> (int)$id
+				));
+			$res = $sth->fetch(PDO::FETCH_OBJ);
+
+			if($res) {
+				$app->response->setStatus(200);
+				$app->response()->headers->set('Content-Type', 'application/json');
+				echo json_encode($res);
+				$db = null;
+			} else {
+				throw new PDOException('No records found.');
+			}
+		} catch (PDOException $e) {
+			$app->response->setStatus(404);
+			echo '{"error":{"text":' . $e->getMessage() . '}}';
+		}
+	}
+
+	/* CREATE */
+	function postLiveMaps() {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+
+		$tab = $app->request->post();
+
+		$table = 'livemaps';//$tab['table'];
+		unset($tab['_METHOD']);
+		unset($tab['table']);
+
+		$keys = implode(',', array_keys($tab));
+        $values = implode(',', array_values($tab));;
+
+		$countKey = count($tab);
+
+        $int = array();
+        for ($i=0; $i < $countKey; $i++) {
+            array_push($int, '?');
+        }
+
+        $interrogation = implode(',', array_values($int));
+
+        try {
+			if($countKey === 0){
+				exit(0);
+			} else {
+				$insert = $db->prepare("INSERT INTO $table ($keys) VALUES($interrogation)");
+				$insert->execute(array_values($tab));
+			}
+			$app->response->setStatus(201);
+			$app->response()->headers->set('Content-Type', 'application/json');
+			echo json_encode(array(
+					"status" => "success", 
+					"code" => 1
+				));
+			$db = null;
+
+		} catch(PDOException $e) {
+			$app->response->setStatus(404);
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
+	/* UPDATE :id */
+	function putLiveMaps($id) {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+
+		$tab = $app->request->put();
+
+		$table = 'livemaps';//$tab['table'];
+		unset($tab['_METHOD']);
+		unset($tab['table']);
+
+        $setter = [];
+        foreach ($tab as $key => $value) {
+        	array_push($setter, $key .' = :'.$key);
+        }
+        $keys = implode(', ', $setter);
+
+		try {
+			$sth = $db->prepare("UPDATE $table SET $keys WHERE id = $id");
+			$sth->execute($tab);
+
+			$app->response->setStatus(200);
+			$app->response()->headers->set('Content-Type', 'application/json');
+			echo json_encode(array(
+					"status" => "success",
+					"code" => 1
+				));
+			$db = null;
+		
+		} catch(PDOException $e) {
+			$app->response->setStatus(404);
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
+	/* DELETE :id */
+	function deleteLiveMaps($id) {
+ 		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+		$tab = $app->request->delete();
+		$table = 'livemaps';//$tab['table'];
+
+ 		try {
+			$sth = $db->prepare("SELECT * FROM $table WHERE id = :id");
+			$sth->bindParam(':id', $id, PDO::PARAM_INT);
+            $sth->execute();
+
+            $res = $sth->fetch(PDO::FETCH_OBJ);
+
+			if($res) {
+				$sth = $db->prepare("DELETE FROM $table WHERE id = :id");
+				$sth->bindParam(':id', $id, PDO::PARAM_INT);
+				$sth->execute();
+
+				$app->response->setStatus(204);
+			} else {
+				throw new PDOException('No records found.');
+			}
+		} catch (PDOException $e) {
+			$app->response->setStatus(400);
+			$app->response()->headers->set('X-Status-Reason', 'application/json');
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*--------------------------------- CLIENTSEARCH --------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+
+	/* GET */
+	function allClientSearch() {
+        $db = getDB();
+ 	 	$app = \Slim\Slim::getInstance();
+
+ 	 	$tab = $app->request->headers;
+		$table = 'clientSearch'; //$tab['table'];
+
+ 	 	try {
+            $sth = $db->prepare("SELECT * FROM $table");
+            $sth->execute();
+            $res = $sth->fetchAll(PDO::FETCH_OBJ);
+
+            if($res){
+	        	$app->response->setStatus(200);
+	        	$app->response()->headers->set('Content-Type', 'application/json');
+	            echo json_encode($res);
+	            $db = null;
+            } else {
+            	throw new PDOException('No records found.');
+            }
+        } catch (PDOException $e) {
+        	$app->response->setStatus(404);
+            echo '{"error":{"text":' . $e->getMessage() . '}}';
+        }
+	}
+
+	/* GET :id */
+	function getClientSearch($id) {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+
+		$tab = $app->request->headers;
+		$table = 'clientSearch';//$tab['table'];
+
+		try {
+			$sth = $db->prepare("SELECT * FROM $table WHERE id = :id");
+			$sth->execute(array(
+					'id'   	=> (int)$id
+				));
+			$res = $sth->fetch(PDO::FETCH_OBJ);
+
+			if($res) {
+				$app->response->setStatus(200);
+				$app->response()->headers->set('Content-Type', 'application/json');
+				echo json_encode($res);
+				$db = null;
+			} else {
+				throw new PDOException('No records found.');
+			}
+		} catch (PDOException $e) {
+			$app->response->setStatus(404);
+			echo '{"error":{"text":' . $e->getMessage() . '}}';
+		}
+	}
+
+	/* CREATE */
+	function postClientSearch() {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+
+		$tab = $app->request->post();
+
+		$table = 'clientSearch';//$tab['table'];
+		unset($tab['_METHOD']);
+		unset($tab['table']);
+
+		$keys = implode(',', array_keys($tab));
+        $values = implode(',', array_values($tab));;
+
+		$countKey = count($tab);
+
+        $int = array();
+        for ($i=0; $i < $countKey; $i++) {
+            array_push($int, '?');
+        }
+
+        $interrogation = implode(',', array_values($int));
+
+        try {
+			if($countKey === 0){
+				exit(0);
+			} else {
+				$insert = $db->prepare("INSERT INTO $table ($keys) VALUES($interrogation)");
+				$insert->execute(array_values($tab));
+			}
+			$app->response->setStatus(201);
+			$app->response()->headers->set('Content-Type', 'application/json');
+			echo json_encode(array(
+					"status" => "success", 
+					"code" => 1
+				));
+			$db = null;
+
+		} catch(PDOException $e) {
+			$app->response->setStatus(404);
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
+	/* UPDATE :id */
+	function putClientSearch($id) {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+
+		$tab = $app->request->put();
+
+		$table = 'clientSearch';//$tab['table'];
+		unset($tab['_METHOD']);
+		unset($tab['table']);
+
+        $setter = [];
+        foreach ($tab as $key => $value) {
+        	array_push($setter, $key .' = :'.$key);
+        }
+        $keys = implode(', ', $setter);
+
+		try {
+			$sth = $db->prepare("UPDATE $table SET $keys WHERE id = $id");
+			$sth->execute($tab);
+
+			$app->response->setStatus(200);
+			$app->response()->headers->set('Content-Type', 'application/json');
+			echo json_encode(array(
+					"status" => "success",
+					"code" => 1
+				));
+			$db = null;
+		
+		} catch(PDOException $e) {
+			$app->response->setStatus(404);
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
+	/* DELETE :id */
+	function deleteClientSearch($id) {
+ 		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+		$tab = $app->request->delete();
+		$table = 'clientSearch';//$tab['table'];
+
+ 		try {
+			$sth = $db->prepare("SELECT * FROM $table WHERE id = :id");
+			$sth->bindParam(':id', $id, PDO::PARAM_INT);
+            $sth->execute();
+
+            $res = $sth->fetch(PDO::FETCH_OBJ);
+
+			if($res) {
+				$sth = $db->prepare("DELETE FROM $table WHERE id = :id");
+				$sth->bindParam(':id', $id, PDO::PARAM_INT);
+				$sth->execute();
+
+				$app->response->setStatus(204);
+			} else {
+				throw new PDOException('No records found.');
+			}
+		} catch (PDOException $e) {
+			$app->response->setStatus(400);
+			$app->response()->headers->set('X-Status-Reason', 'application/json');
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------- CUSTOM FEATURE -------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------*/
+
+	/* CUSTOM FEATURE */
+	function remove() {
+		$db = getDB();
+		$app = \Slim\Slim::getInstance();
+		$tab = $app->request->delete();
+		$table = 'livemaps';//$tab['table'];
+
+		// On supprime les membres qui ne sont pas sur la page donc qui n'ont pas actualisé automatiquement cet fonction
+ 		try {
+			$sth = $db->prepare("DELETE FROM $table WHERE timestamp < :timenow");
+			$sth->execute(array(
+				'timenow' => time()-20
+			));
+
+			$app->response->setStatus(204);
+		} catch (PDOException $e) {
+			$app->response->setStatus(400);
+			$app->response()->headers->set('X-Status-Reason', 'application/json');
+			echo json_encode(array(
+					"error" => $e->getMessage(), 
+					"code" => 2
+				));
+		}
+	}
+
 ?>
